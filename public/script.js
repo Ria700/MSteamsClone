946b5fe15891b0c5d14a15002586049aece4dc58
Ria700 <ria.monga07@gmail.com>
2021-06-17 20:35:00 +0530

    View video

--- public/script.js
+++ public/script.js
@@ -1,7 +1,19 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true // Mute video for ourselves
+
+// Connect our video
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => { //Promise
+    // myVideo = stream
+    addVideoStream(myVideo, stream)
 })
 
 myPeer.on('open', id => {
@@ -11,3 +23,11 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId) 
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadmetadata', () => {
+        video.play() // Play the video
+    })
+    videoGrid.append(video)
+}
