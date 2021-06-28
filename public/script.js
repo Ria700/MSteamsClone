f0c6064fc2346ec683552e840a9b1d6d5bab162f
Ria700 <ria.monga07@gmail.com>
2021-06-28 20:25:56 +0530

    changed url

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure:true,
-    host: 'https://blooming-lowlands-02462.herokuapp.com/',
+    host: "https://clonemsteams.herokuapp.com/",
     port: '443',
     path: "/peerjs"
 })
