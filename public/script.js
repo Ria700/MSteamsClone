fb587a30362e9e51b667b48cc52a8d1f2f7a981d
Ria700 <ria.monga07@gmail.com>
2021-06-28 18:27:22 +0530

    some msg

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure: true,
-    host: 'https://msteamsclone7.herokuapp.com/',
+    host: '/',
     port: '443',
     path: "/peerjs",
 })
