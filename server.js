c3847589aab809e632e1a5cc214a0f5c1ab38ad4
Ria700 <ria.monga07@gmail.com>
2021-06-28 20:12:34 +0530

    change

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ io.on('connection', socket => {
     })
 })
 
-var ExpressPeerServer = require("peer").ExpressPeerServer;    
-var options = {
-  debug: true,
-  allow_discovery: true,
-};
-let peerServer = ExpressPeerServer(server, options);
-app.use("/peerjs", peerServer);
+// var ExpressPeerServer = require("peer").ExpressPeerServer;    
+// var options = {
+//   debug: true,
+//   allow_discovery: true,
+// };
+// let peerServer = ExpressPeerServer(server, options);
+// app.use("/peerjs", peerServer);
 
 server.listen(process.env.PORT||3000)
