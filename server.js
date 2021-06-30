3dcd6d23da2d8c96762f50e2c1fdb45362135727
Ria700 <ria.monga07@gmail.com>
2021-06-30 20:29:28 +0530

    vc chat working

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-})
+// const { ExpressPeerServer } = require('peer');
+// const peerServer = ExpressPeerServer(server, {
+//     debug: true
+// })
 const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer)
+// app.use('/peerjs', peerServer)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -22,10 +22,10 @@ app.get('/:room', (req, res) => {
 })
 
 // var ExpressPeerServer = require("peer").ExpressPeerServer;    
-var options = {
-    debug: true,
-    allow_discovery: true,
-};
+// var options = {
+//     debug: true,
+//     allow_discovery: true,
+// }
 // let peerServer = ExpressPeerServer(server, options);
 // app.use("/peerjs", peerServer);
 
