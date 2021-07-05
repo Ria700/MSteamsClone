acbb05811b0535b484cb19e3f3ab5ad3bab2bb5f
Ria700 <ria.monga07@gmail.com>
2021-07-05 23:37:16 +0530

    back

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const app = express()
-var ExpressPeerServer = require("peer").ExpressPeerServer;    
-var options = {
-    debug: true,
-    // allow_discovery: true,
-}
 const server = require('http').Server(app)
-
-app.use('/peerjs', ExpressPeerServer(server, options));
-
 const io = require('socket.io')(server)
 // const { ExpressPeerServer } = require('peer');
 // const peerServer = ExpressPeerServer(server, {
@@ -16,7 +8,7 @@ const io = require('socket.io')(server)
 // })
 const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer)
+// app.use('/peerjs', peerServer)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -29,7 +21,11 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
 
-
+// var ExpressPeerServer = require("peer").ExpressPeerServer;    
+// var options = {
+//     debug: true,
+//     allow_discovery: true,
+// }
 // let peerServer = ExpressPeerServer(server, options);
 // app.use("/peerjs", peerServer);
 
