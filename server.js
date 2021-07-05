060aaf2c31e0c0ce0ce3815f7b2b107dfa6c753a
Ria700 <ria.monga07@gmail.com>
2021-07-05 23:07:02 +0530

    server changed

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-server.listen(PORT, () => console.log(`Server Started on ${PORT}`))
+server.listen(port, () => console.log(`Server Started on ${port}`))
