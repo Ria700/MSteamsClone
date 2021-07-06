69f6267fab7127e4fe92bbfb0f03db2989100318
Ria700 <ria.monga07@gmail.com>
2021-07-06 19:53:35 +0530

    ui

--- public/script.js
+++ public/script.js
@@ -111,7 +111,6 @@ const playStop = () => {
 const setMuteButton = () => {
     const html = `
     <i class="fas fa-microphone"></i>
-    <span>Mute</span>
   `
     document.querySelector('.main__mute_button').innerHTML = html;
 }
@@ -119,7 +118,6 @@ const setMuteButton = () => {
 const setUnmuteButton = () => {
     const html = `
     <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
   `
     document.querySelector('.main__mute_button').innerHTML = html;
 }
@@ -127,7 +125,6 @@ const setUnmuteButton = () => {
 const setStopVideo = () => {
     const html = `
     <i class="fas fa-video"></i>
-    <span>Stop Video</span>
   `
     document.querySelector('.main__video_button').innerHTML = html;
 }
@@ -135,7 +132,6 @@ const setStopVideo = () => {
 const setPlayVideo = () => {
     const html = `
   <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
   `
     document.querySelector('.main__video_button').innerHTML = html;
 }
